56af46ce6c9a363cd676757260c90e019e3ec6b5
Shiv Thakur <125086310+shivthakur007@users.noreply.github.com>
2026-02-15 23:08:33 +0530

    Update firebase_config.py

--- expense_tracker/firebase_config.py
+++ expense_tracker/firebase_config.py
@@ -1,12 +1,10 @@
-import json
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
 
 def get_db():
     if not firebase_admin._apps:
-        firebase_dict = json.loads(st.secrets["firebase"]["credentials"])
-        cred = credentials.Certificate(firebase_dict)
+        cred = credentials.Certificate(dict(st.secrets["firebase"]))
         firebase_admin.initialize_app(cred)
 
     return firestore.client()
